85b087d1943fb270336b73dbdbfd98e7a71417f1
xingyinzhu <xingyinzhu@gmail.com>
2014-08-23 22:23:03 +0800

    first commit

---
+++ app/views/emails/contactemail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Contact Form</h2>
+		<div>
+			Someone just contacted us:
+		</div>
+		<div> Subject: {{ $subject }} </div>
+		<div> Message: {{ $emailmessage }} </div>
+	</body>
+</html>
+
